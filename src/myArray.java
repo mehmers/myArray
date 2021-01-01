6a743fe3ef2f201f7c50ff000d8e3d9a0902f11f
mehmers <67421910+mehmers@users.noreply.github.com>
2021-01-01 02:11:49 +0200

    Create myArray.java

---
+++ src/myArray.java
@@ -0,0 +1,50 @@
+public class myArray<T>{
+
+  private int size;
+  private T[] array = (T[]) new Object[size];
+
+  public myArray(int size){
+    this.size = size;
+  }
+
+  public void setArray(T[] newArray){
+    this.array = newArray;
+  }
+
+  public T[] getArray(){
+    return this.array;
+  }
+
+  public void push(T object){
+    int length = this.array.length;
+    T[] tempArray = (T[]) new Object[length+1];
+    for(int i = 0 ; i < length ; i++){
+      tempArray[i] = array[i];
+    }
+    tempArray[length] = object;
+    this.array = tempArray;
+  }
+
+  public void remove(int index){
+    int length = this.array.length;
+    int count = 0;
+    T[] tempArray = (T[]) new Object[length-1];
+    for(int i = 0 ; i < length ; i++){
+      if(i != index){
+        tempArray[count] = array[i];
+      }
+    }
+    this.array = tempArray;
+  }
+
+  public boolean contains(Object a){
+    boolean result = false;
+    int length = this.array.length;
+    for(int i = 0; i < length ; i++){
+      if(array[i] == a){
+        result = true;
+      }
+    }
+    return result;
+  }
+}
